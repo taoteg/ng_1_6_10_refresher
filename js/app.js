daf7d43cc6cb0c539047a0b4327943776d41382b
taoteg <taoteg@gmail.com>
2018-07-05 14:45:06 -0500

    Section 3: Common Directives

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@
 var app = angular.module('firstNgApp', []);
 
 app.controller('mainCtrl', function() {
+  // Part 2.
   this.world = 'hello';
   this.inputtext = 'some text to start...';
+
+  // Part 3.
+  this.fruits = ['apples', 'oranges','bananas'];
+
+  this.alertMe = function() {
+    alert('something');
+  }
 });

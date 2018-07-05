68a2dfc091e8920203069d0b4bc2bb71eea05270
taoteg <taoteg@gmail.com>
2018-07-05 15:12:18 -0500

    Part 4: Filters.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,23 @@
 var app = angular.module('firstNgApp', []);
 
 app.controller('mainCtrl', function() {
-  // Part 2.
+  // Part 2. Two-way Data Binding & Controllers.
   this.world = 'hello';
   this.inputtext = 'some text to start...';
 
-  // Part 3.
+  // Part 3. Directives.
   this.fruits = ['apples', 'oranges','bananas'];
 
   this.alertMe = function() {
     alert('something');
+  }
+
+  // Part 4. Filters.
+  this.cars = ['oldmosbile', 'hyundai', 'kia', 'ford', 'honda', 'kawasaki'];
+});
+
+app.filter('makePlural', function() {
+  return function(input) {
+    return input + 's';
   }
 });

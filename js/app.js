beb91cbf396cc7b622334bda89c1e12989498162
taoteg <taoteg@gmail.com>
2018-07-05 15:37:41 -0500

    Part 5:  service usage.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Base Javascript file for app.
 var app = angular.module('firstNgApp', []);
 
-app.controller('mainCtrl', function() {
+app.controller('mainCtrl', function($http) {
   // Part 2. Two-way Data Binding & Controllers.
   this.world = 'hello';
   this.inputtext = 'some text to start...';
@@ -15,6 +15,15 @@ app.controller('mainCtrl', function() {
 
   // Part 4. Filters.
   this.cars = ['oldmosbile', 'hyundai', 'kia', 'ford', 'honda', 'kawasaki'];
+
+  // Part 5: $http Service.
+  this.animals = ['cat', 'dog', 'rabbit', 'bird', 'horse', 'snake'];
+  var targetUrl = 'https://jsonplaceholder.typicode.com/posts';
+  var vm = this;
+  $http.get(targetUrl).then(function(response) {
+    // console.log(response);
+    vm.posts = response.data;
+  });
 });
 
 app.filter('makePlural', function() {

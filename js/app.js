42f0d54acc25e23730f0cb11e364e986a6f0f0e9
taoteg <taoteg@gmail.com>
2018-07-05 16:05:28 -0500

    Part 6A and B: Services and Factories.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // Base Javascript file for app.
 var app = angular.module('firstNgApp', []);
 
-app.controller('mainCtrl', function($http) {
+app.controller('mainCtrl', function( $http, albumSvc, commentSvc ) {
+
   // Part 2. Two-way Data Binding & Controllers.
   this.world = 'hello';
   this.inputtext = 'some text to start...';
@@ -17,17 +18,54 @@ app.controller('mainCtrl', function($http) {
   this.cars = ['oldmosbile', 'hyundai', 'kia', 'ford', 'honda', 'kawasaki'];
 
   // Part 5: $http Service.
-  this.animals = ['cat', 'dog', 'rabbit', 'bird', 'horse', 'snake'];
-  var targetUrl = 'https://jsonplaceholder.typicode.com/posts';
+  // declaring vm as `this` is required to get the correct reference to `this` in the $http.get method.
   var vm = this;
+  var targetUrl = 'https://jsonplaceholder.typicode.com/posts';
   $http.get(targetUrl).then(function(response) {
     // console.log(response);
     vm.posts = response.data;
   });
+
+  // Part 6A. Services.
+  albumSvc.getAlbums().then(function(response) {
+    // console.log(response);
+    vm.albums = response.data;
+  });
+
+  // Part 6B. Factories.
+  commentSvc.getComments().then(function(response) {
+    // console.log(response);
+    vm.comments = response.data;
+  });
 });
 
+// Part 4 (continued)
 app.filter('makePlural', function() {
   return function(input) {
     return input + 's';
+  }
+});
+
+// Part 6A: Services.
+app.service('albumSvc', function($http) {
+  // Relocating get call into a service.
+  var targetUrl = 'https://jsonplaceholder.typicode.com/albums';
+
+  this.getAlbums = function() {
+    return $http.get(targetUrl);
+  }
+});
+
+// Part 6B: Factory.
+app.factory('commentSvc', function($http) {
+  // Relocating get call into a service.
+  var targetUrl = 'https://jsonplaceholder.typicode.com/comments';
+
+  var getComments = function() {
+    return $http.get(targetUrl);
+  };
+
+  return {
+    getComments: getComments
   }
 });
